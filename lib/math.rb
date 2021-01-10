4372672d7a4615b20ff491aacf249117ee393e08
Marnie Robinson <marnielane@gmail.com>
2021-01-10 20:09:27 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,10 +7,12 @@ end
 puts addition(5,4)
 
 def subtraction(num1, num2)
-  sum = #{num1} - #{num2}
+  sum = num1 - num2
   puts sum 
   sum
 end
+
+puts subtraction(10,4)
 
 def division(num1, num2)
   sum = #{num1} / #{num2}

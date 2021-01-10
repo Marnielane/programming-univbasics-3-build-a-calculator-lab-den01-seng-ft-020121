18bb6edfa53f77a044cf097b0e54f9943d7b53f7
Marnie Robinson <marnielane@gmail.com>
2021-01-10 20:37:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -38,11 +38,11 @@ end
 
 puts modulo(81,9)
 
-def square_root(num1)
+def square_root(num2)
   num2 = num1 * num1
   sum = num2 / num1
   puts sum
   sum
 end
 
-puts square_root(9)
+puts square_root(81)

fecdeeaadf4d65719c0f2b5829ac93f65270326d
Marnie Robinson <marnielane@gmail.com>
2021-01-10 20:06:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,37 @@
-def addition(num1, num2)
-
+def addition(num1 = 5, num2 = 4)
+  sum = #{num1} + #{num2}
+  puts sum
+  return sum
 end
 
-def subtraction(num1, num2)
+addition(5,4)
 
+def subtraction(num1, num2)
+  sum = #{num1} - #{num2}
+  puts sum 
+  sum
 end
 
 def division(num1, num2)
-
+  sum = #{num1} / #{num2}
+  puts sum
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = #{num1} * #{num2}
+  puts sum
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = #{num1} % #{num2}
+  puts sum
+  sum
 end
 
 def square_root(num)
-
+  sum = #{num} **
+  puts sum
+  sum
 end
